46b4f5de6067e38a392f4151f40998df5114c028
Sun He <sunheehnus@gmail.com>
2016-02-05 01:23:31 +0800

    add test code

---
+++ Makefile
@@ -0,0 +1,85 @@
+# STANDALONE REDIS NODE
+define REDIS_STANDALONE_NODE_CONF
+daemonize yes
+port 6379
+pidfile /tmp/redis_standalone_node_for_spark-redis.pid
+logfile /tmp/redis_standalone_node_for_spark-redis.log
+save ""
+appendonly no
+endef
+
+# CLUSTER REDIS NODES
+define REDIS_CLUSTER_NODE1_CONF
+daemonize yes
+port 7379
+pidfile /tmp/redis_cluster_node1_for_spark-redis.pid
+logfile /tmp/redis_cluster_node1_for_spark-redis.log
+save ""
+appendonly no
+cluster-enabled yes
+cluster-config-file /tmp/redis_cluster_node1_for_spark-redis.conf
+endef
+
+define REDIS_CLUSTER_NODE2_CONF
+daemonize yes
+port 7380
+pidfile /tmp/redis_cluster_node2_for_spark-redis.pid
+logfile /tmp/redis_cluster_node2_for_spark-redis.log
+save ""
+appendonly no
+cluster-enabled yes
+cluster-config-file /tmp/redis_cluster_node2_for_spark-redis.conf
+endef
+
+define REDIS_CLUSTER_NODE3_CONF
+daemonize yes
+port 7381
+pidfile /tmp/redis_cluster_node3_for_spark-redis.pid
+logfile /tmp/redis_cluster_node3_for_spark-redis.log
+save ""
+appendonly no
+cluster-enabled yes
+cluster-config-file /tmp/redis_cluster_node3_for_spark-redis.conf
+endef
+
+export REDIS_STANDALONE_NODE_CONF
+export REDIS_CLUSTER_NODE1_CONF
+export REDIS_CLUSTER_NODE2_CONF
+export REDIS_CLUSTER_NODE3_CONF
+
+start:
+	echo "$$REDIS_STANDALONE_NODE_CONF" | redis-server -
+	echo "$$REDIS_CLUSTER_NODE1_CONF" | redis-server -
+	echo "$$REDIS_CLUSTER_NODE2_CONF" | redis-server -
+	echo "$$REDIS_CLUSTER_NODE3_CONF" | redis-server -
+	redis-cli -p 7380 cluster meet 127.0.0.1 7379 > /dev/null
+	redis-cli -p 7381 cluster meet 127.0.0.1 7379 > /dev/null
+	slots=$$(seq 0 5460); slots=$$(echo $$slots | tr '\n' ' '); redis-cli -p 7379 cluster addslots $$slots > /dev/null
+	slots=$$(seq 5461 10922); slots=$$(echo $$slots | tr '\n' ' '); redis-cli -p 7380 cluster addslots $$slots > /dev/null
+	slots=$$(seq 10923 16383); slots=$$(echo $$slots | tr '\n' ' '); redis-cli -p 7381 cluster addslots $$slots > /dev/null
+
+stop:
+	kill `cat /tmp/redis_standalone_node_for_spark-redis.pid`
+	kill `cat /tmp/redis_cluster_node1_for_spark-redis.pid` || true
+	kill `cat /tmp/redis_cluster_node2_for_spark-redis.pid` || true
+	kill `cat /tmp/redis_cluster_node3_for_spark-redis.pid` || true
+	rm -f /tmp/redis_cluster_node1_for_spark-redis.conf
+	rm -f /tmp/redis_cluster_node2_for_spark-redis.conf
+	rm -f /tmp/redis_cluster_node3_for_spark-redis.conf
+
+test:
+	make start
+	mvn -Dtest=${TEST} clean compile test
+	make stop
+
+deploy:
+	make start
+	mvn clean deploy
+	make stop
+
+package:
+	make start
+	mvn clean package
+	make stop
+
+.PHONY: test
---
+++ src/test/scala/com/redislabs/provider/redis/rdd/RedisRDDSuite.scala
@@ -0,0 +1,123 @@
+package com.redislabs.provider.redis.rdd
+
+import org.apache.spark.{SparkContext, SparkConf}
+import org.scalatest.{BeforeAndAfterAll, ShouldMatchers, FunSuite}
+import scala.io.Source.fromInputStream
+import com.redislabs.provider.redis._
+import org.scalatest.Matchers._
+
+class RedisRDDSuite extends FunSuite with ENV with BeforeAndAfterAll with ShouldMatchers {
+  override def beforeAll() {
+    super.beforeAll()
+
+    sc = new SparkContext(new SparkConf().setMaster("local").setAppName(getClass.getName))
+    redisStandaloneDB = ("127.0.0.1", 6379)
+    redisClusterDB = ("127.0.0.1", 7379)
+    content = fromInputStream(getClass.getClassLoader.getResourceAsStream("blog")).
+      getLines.toArray.mkString("\n")
+
+    val wcnts = sc.parallelize(content.split("\\W+").filter(!_.isEmpty)).map((_, 1)).
+      reduceByKey(_ + _).map(x => (x._1, x._2.toString))
+    val wds = sc.parallelize(content.split("\\W+").filter(!_.isEmpty))
+
+    sc.toRedisKV(wcnts, redisStandaloneDB)
+    sc.toRedisZSET(wcnts, "all:words:cnt:sortedset", redisStandaloneDB)
+    sc.toRedisHASH(wcnts, "all:words:cnt:hash", redisStandaloneDB)
+    sc.toRedisLIST(wds, "all:words:list", redisStandaloneDB)
+    sc.toRedisSET(wds, "all:words:set", redisStandaloneDB)
+
+    sc.toRedisKV(wcnts, redisClusterDB)
+    sc.toRedisZSET(wcnts, "all:words:cnt:sortedset", redisClusterDB)
+    sc.toRedisHASH(wcnts, "all:words:cnt:hash", redisClusterDB)
+    sc.toRedisLIST(wds, "all:words:list", redisClusterDB)
+    sc.toRedisSET(wds, "all:words:set", redisClusterDB)
+  }
+
+  test("RedisKVRDD - standalone") {
+    val redisKVRDD = sc.fromRedisKeyPattern(redisStandaloneDB, "*").getKV
+    val kvContents = redisKVRDD.sortByKey().collect
+    val wcnts = content.split("\\W+").filter(!_.isEmpty).map((_, 1)).groupBy(_._1).
+      map(x => (x._1, x._2.map(_._2).reduce(_ + _).toString)).toArray.sortBy(_._1)
+    kvContents should be (wcnts)
+  }
+
+  test("RedisKVRDD - cluster") {
+    val redisKVRDD = sc.fromRedisKeyPattern(redisClusterDB, "*").getKV
+    val kvContents = redisKVRDD.sortByKey().collect
+    val wcnts = content.split("\\W+").filter(!_.isEmpty).map((_, 1)).groupBy(_._1).
+      map(x => (x._1, x._2.map(_._2).reduce(_ + _).toString)).toArray.sortBy(_._1)
+    kvContents should be (wcnts)
+  }
+
+  test("RedisZsetRDD - standalone") {
+    val redisZSetRDD = sc.fromRedisKeyPattern(redisStandaloneDB, "all:words:cnt:sortedset").getZSet
+    val zsetContents = redisZSetRDD.sortByKey().collect
+    val wcnts = content.split("\\W+").filter(!_.isEmpty).map((_, 1)).groupBy(_._1).
+      map(x => (x._1, x._2.map(_._2).reduce(_ + _).toString + ".0")).toArray.sortBy(_._1)
+    zsetContents should be (wcnts)
+  }
+
+  test("RedisZsetRDD - cluster") {
+    val redisZSetRDD = sc.fromRedisKeyPattern(redisClusterDB, "all:words:cnt:sortedset").getZSet
+    val zsetContents = redisZSetRDD.sortByKey().collect
+    val wcnts = content.split("\\W+").filter(!_.isEmpty).map((_, 1)).groupBy(_._1).
+      map(x => (x._1, x._2.map(_._2).reduce(_ + _).toString + ".0")).toArray.sortBy(_._1)
+    zsetContents should be (wcnts)
+  }
+
+  test("RedisHashRDD - standalone") {
+    val redisHashRDD = sc.fromRedisKeyPattern(redisStandaloneDB, "all:words:cnt:hash").getHash
+    val hashContents = redisHashRDD.sortByKey().collect
+    val wcnts = content.split("\\W+").filter(!_.isEmpty).map((_, 1)).groupBy(_._1).
+      map(x => (x._1, x._2.map(_._2).reduce(_ + _).toString)).toArray.sortBy(_._1)
+    hashContents should be (wcnts)
+  }
+
+  test("RedisHashRDD - cluster") {
+    val redisHashRDD = sc.fromRedisKeyPattern(redisClusterDB, "all:words:cnt:hash").getHash
+    val hashContents = redisHashRDD.sortByKey().collect
+    val wcnts = content.split("\\W+").filter(!_.isEmpty).map((_, 1)).groupBy(_._1).
+      map(x => (x._1, x._2.map(_._2).reduce(_ + _).toString)).toArray.sortBy(_._1)
+    hashContents should be (wcnts)
+  }
+
+  test("RedisListRDD - standalone") {
+    val redisListRDD = sc.fromRedisKeyPattern(redisStandaloneDB, "all:words:list").getList
+    val listContents = redisListRDD.sortBy(x => x).collect
+    val ws = content.split("\\W+").filter(!_.isEmpty).sorted
+    listContents should be (ws)
+  }
+
+  test("RedisListRDD - cluster") {
+    val redisListRDD = sc.fromRedisKeyPattern(redisClusterDB, "all:words:list").getList
+    val listContents = redisListRDD.sortBy(x => x).collect
+    val ws = content.split("\\W+").filter(!_.isEmpty).sorted
+    listContents should be (ws)
+  }
+
+  test("RedisSetRDD - standalone") {
+    val redisSetRDD = sc.fromRedisKeyPattern(redisStandaloneDB, "all:words:set").getSet
+    val setContents = redisSetRDD.sortBy(x => x).collect
+    val ws = content.split("\\W+").filter(!_.isEmpty).distinct.sorted
+    setContents should be (ws)
+  }
+
+  test("RedisSetRDD - cluster") {
+    val redisSetRDD = sc.fromRedisKeyPattern(redisClusterDB, "all:words:set").getSet
+    val setContents = redisSetRDD.sortBy(x => x).collect
+    val ws = content.split("\\W+").filter(!_.isEmpty).distinct.sorted
+    setContents should be (ws)
+  }
+
+  override def afterAll(): Unit = {
+    sc.stop
+    System.clearProperty("spark.driver.port")
+  }
+}
+
+trait ENV {
+  var sc: SparkContext = _
+  var redisStandaloneDB: (String, Int) = _
+  var redisClusterDB: (String, Int) = _
+  var content: String = _
+}
