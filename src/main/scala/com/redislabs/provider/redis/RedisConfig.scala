d5bc746d89633f2e41bd87bca7cbe86ab42369cb
Sun He <sunheehnus@gmail.com>
2016-03-12 10:11:38 +0800

    cluster_enable -> cluater_enabled

--- src/main/scala/com/redislabs/provider/redis/RedisConfig.scala
+++ src/main/scala/com/redislabs/provider/redis/RedisConfig.scala
@@ -145,7 +145,7 @@ class RedisConfig(val initialHost: RedisEndpoint) extends  Serializable {
     val conn = initialHost.connect()
     val info = conn.info.split("\n")
     val version = info.filter(_.contains("redis_version:"))(0)
-    val clusterEnable = info.filter(_.contains("cluster_enable:"))
+    val clusterEnable = info.filter(_.contains("cluster_enabled:"))
     val mainVersion = version.substring(14, version.indexOf(".")).toInt
     val res = mainVersion>2 && clusterEnable.length>0 && clusterEnable(0).contains("1")
     conn.close
