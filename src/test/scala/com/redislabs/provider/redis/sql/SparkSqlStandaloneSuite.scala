d47f9629398565e2083a32d8ebd51dd46e2b7252
The Viet Nguyen <ntviet18@gmail.com>
2018-09-29 20:11:39 +0300

    Test insert overwrite with existing data

--- src/test/scala/com/redislabs/provider/redis/sql/SparkSqlStandaloneSuite.scala
+++ src/test/scala/com/redislabs/provider/redis/sql/SparkSqlStandaloneSuite.scala
@@ -9,9 +9,8 @@ import org.scalatest.Matchers
   */
 class SparkSqlStandaloneSuite extends RedisStandaloneSuite with DefaultTestDataset with Matchers {
 
-  test("create table") {
+  test("create table then make regular insertions") {
     val tableName = Person.generatePersonTableName()
-    val df = Person.df(spark)
     // TODO: retrieve table name from tableName instead of path option
     spark.sql(
       s"""CREATE TABLE $tableName (name STRING, age INT, address STRING, salary DOUBLE)
@@ -20,6 +19,27 @@ class SparkSqlStandaloneSuite extends RedisStandaloneSuite with DefaultTestDatas
     spark.sql(
       s"""INSERT INTO TABLE $tableName
          |  VALUES ('John', 30, '60 Wall Street', 150.5),
+         |    ('Peter', 35, '110 Wall Street', 200.3)
+         |""".stripMargin)
+    val loadedDf = spark.read.format(RedisFormat).load(tableName).cache()
+    verifyDf(loadedDf)
+  }
+
+  test("create table then make overwrite insertions when data exists") {
+    val tableName = Person.generatePersonTableName()
+    // TODO: retrieve table name from tableName instead of path option
+    spark.sql(
+      s"""CREATE TABLE $tableName (name STRING, age INT, address STRING, salary DOUBLE)
+         |  USING $RedisFormat OPTIONS (path '$tableName')
+         |""".stripMargin)
+    spark.sql(
+      s"""INSERT INTO TABLE $tableName
+         |  VALUES ('Johnny', 18, '17 Home Street', 0),
+         |    ('Peter', 23, '6 Home Street', 20)
+         |""".stripMargin)
+    spark.sql(
+      s"""INSERT overwrite TABLE $tableName
+         |SELECT * FROM VALUES ('John', 30, '60 Wall Street', 150.5),
          |    ('Peter', 35, '110 Wall Street', 200.3)
          |""".stripMargin)
     val loadedDf = spark.read.format(RedisFormat).load(tableName).cache()
