de480fa82020e54355beba1b0e4afeee27ac4b14
The Viet Nguyen <ntviet18@gmail.com>
2018-09-29 20:12:17 +0300

    Test insert overwrite with no existing data

--- src/test/scala/com/redislabs/provider/redis/sql/SparkSqlStandaloneSuite.scala
+++ src/test/scala/com/redislabs/provider/redis/sql/SparkSqlStandaloneSuite.scala
@@ -25,6 +25,22 @@ class SparkSqlStandaloneSuite extends RedisStandaloneSuite with DefaultTestDatas
     verifyDf(loadedDf)
   }
 
+  test("create table then make overwrite insertions when no data exists") {
+    val tableName = Person.generatePersonTableName()
+    // TODO: retrieve table name from tableName instead of path option
+    spark.sql(
+      s"""CREATE TABLE $tableName (name STRING, age INT, address STRING, salary DOUBLE)
+         |  USING $RedisFormat OPTIONS (path '$tableName')
+         |""".stripMargin)
+    spark.sql(
+      s"""INSERT overwrite TABLE $tableName
+         |SELECT * FROM VALUES ('John', 30, '60 Wall Street', 150.5),
+         |    ('Peter', 35, '110 Wall Street', 200.3)
+         |""".stripMargin)
+    val loadedDf = spark.read.format(RedisFormat).load(tableName).cache()
+    verifyDf(loadedDf)
+  }
+
   test("create table then make overwrite insertions when data exists") {
     val tableName = Person.generatePersonTableName()
     // TODO: retrieve table name from tableName instead of path option
