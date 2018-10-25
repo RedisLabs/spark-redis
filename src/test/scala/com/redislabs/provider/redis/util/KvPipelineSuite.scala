80cac05e1de7a6bbfa7734b6036a4b2b40ea1504
The Viet Nguyen <ntviet18@gmail.com>
2018-10-26 01:35:36 +0300

    Cleanup k/v hgetall test

--- src/test/scala/com/redislabs/provider/redis/util/KvPipelineSuite.scala
+++ src/test/scala/com/redislabs/provider/redis/util/KvPipelineSuite.scala
@@ -2,6 +2,7 @@ package com.redislabs.provider.redis.util
 
 import java.util.{Collections, UUID}
 
+import com.redislabs.provider.redis.util.ConnectionUtils.withConnection
 import com.redislabs.provider.redis.{RedisConfig, RedisEndpoint}
 import org.scalatest.{FunSuite, Matchers}
 import redis.clients.jedis.Implicits.KvPipeline
@@ -18,16 +19,15 @@ class KvPipelineSuite extends FunSuite with Matchers {
   val redisConfig: RedisConfig =
     new RedisConfig(RedisEndpoint(redisHost, redisPort, redisAuth))
 
-  test("k/v pipeline") {
-    val host = redisConfig.initialHost
-    val conn = host.connect()
-    val pipeline = conn.pipelined()
-    val key = UUID.randomUUID().toString
-    val hash = Collections.singletonMap("key", "value")
-    pipeline.hmset(key, hash)
-    pipeline.getHashAllWithKey(key)
-    val results = pipeline.syncAndReturnAll()
-    results should contain(key -> hash)
-    conn.close()
+  test("k/v hgetall") {
+    withConnection(redisConfig.initialHost) { conn =>
+      val pipeline = conn.pipelined()
+      val key = UUID.randomUUID().toString
+      val hash = Collections.singletonMap("key", "value")
+      pipeline.hmset(key, hash)
+      pipeline.getHashAllWithKey(key)
+      val results = pipeline.syncAndReturnAll()
+      results should contain(key -> hash)
+    }
   }
 }
