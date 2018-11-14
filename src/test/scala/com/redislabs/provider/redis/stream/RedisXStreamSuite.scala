fc69cf707d1d24d4207e8929c42e7bc8d5ed646c
fe2s <aka.fe2s@gmail.com>
2018-11-14 16:33:43 +0200

    reworked streaming test to store retrieved items in the list, so that we check ordering and duplicates

--- src/test/scala/com/redislabs/provider/redis/stream/RedisXStreamSuite.scala
+++ src/test/scala/com/redislabs/provider/redis/stream/RedisXStreamSuite.scala
@@ -1,6 +1,7 @@
 package com.redislabs.provider.redis.stream
 
 import com.redislabs.provider.redis.streaming.{ConsumerConfig, Earliest}
+import com.redislabs.provider.redis.util.ConnectionUtils.withConnection
 import com.redislabs.provider.redis.util.TestUtils
 import com.redislabs.provider.redis.{SparkStreamingRedisSuite, _}
 import org.apache.spark.storage.StorageLevel
@@ -24,33 +25,38 @@ trait RedisXStreamSuite extends SparkStreamingRedisSuite with Matchers {
 
     val _redisConfig = redisConfig // to make closure serializable
 
-    // iterate over items and save to redis as K/V
+    // iterate over items and save to redis list
     stream.foreachRDD { rdd =>
-      rdd.foreach { item =>
-        val k = s"${item.streamKey}:${item.id.v1}-${item.id.v2}"
-        val jedis = _redisConfig.connectionForKey(k)
-        jedis.set(k, item.fields.mkString(" "))
+      rdd.foreachPartition { partition =>
+        for (item <- partition) {
+          val listKey = s"${item.streamKey}:list"
+          withConnection(_redisConfig.connectionForKey(listKey)) { conn =>
+            conn.rpush(listKey, s"${item.id.v1}-${item.id.v2} " + item.fields.mkString(" "))
+          }
+        }
       }
     }
 
     // write to stream
-    val jedis = redisConfig.connectionForKey(streamKey)
-    jedis.xadd(streamKey, new EntryID(1, 0), Map("a" -> "1", "z" -> "4"))
-    jedis.xadd(streamKey, new EntryID(1, 1), Map("b" -> "2"))
-    jedis.xadd(streamKey, new EntryID(2, 0), Map("c" -> "3"))
+    withConnection(redisConfig.connectionForKey(streamKey)) { conn =>
+      conn.xadd(streamKey, new EntryID(1, 0), Map("a" -> "1", "z" -> "4"))
+      conn.xadd(streamKey, new EntryID(1, 1), Map("b" -> "2"))
+      conn.xadd(streamKey, new EntryID(2, 0), Map("c" -> "3"))
+    }
 
     ssc.start()
 
-    // eventually there should be KVs
-    eventually {
-      jedisGet(s"$streamKey:1-0") shouldBe "a -> 1 z -> 4"
-      jedisGet(s"$streamKey:1-1") shouldBe "b -> 2"
-      jedisGet(s"$streamKey:2-0") shouldBe "c -> 3"
+    // eventually there should be items in the list
+    val listKey = s"$streamKey:list"
+    withConnection(redisConfig.connectionForKey(listKey)) { conn =>
+      eventually {
+        conn.llen(listKey) shouldBe 3
+        conn.lpop(listKey) should be("1-0 a -> 1 z -> 4")
+        conn.lpop(listKey) should be("1-1 b -> 2")
+        conn.lpop(listKey) should be("2-0 c -> 3")
+      }
     }
-  }
 
-  def jedisGet(key: String): String = {
-    redisConfig.connectionForKey(key).get(key)
   }
 
 }
