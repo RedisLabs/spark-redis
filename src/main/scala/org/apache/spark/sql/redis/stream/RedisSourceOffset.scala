67ecfe3d1b997859055e314b39498659b9738fa7
The Viet Nguyen <ntviet18@gmail.com>
2018-12-01 16:34:30 +0200

    Read Redis source offset from json

--- src/main/scala/org/apache/spark/sql/redis/stream/RedisSourceOffset.scala
+++ src/main/scala/org/apache/spark/sql/redis/stream/RedisSourceOffset.scala
@@ -2,13 +2,24 @@ package org.apache.spark.sql.redis.stream
 
 import com.redislabs.provider.redis.util.JsonUtils
 import org.apache.spark.sql.execution.streaming.Offset
+import org.json4s.jackson.Serialization
+import org.json4s.{Formats, NoTypeHints}
 
 /**
   * @author The Viet Nguyen
   */
 case class RedisSourceOffset(offsets: Map[String, RedisConsumerOffset]) extends Offset {
 
-  override def json(): String = JsonUtils.toJson(offsets)
+  override def json(): String = JsonUtils.toJson(this)
+}
+
+object RedisSourceOffset {
+
+  private implicit val formats: Formats = Serialization.formats(NoTypeHints)
+
+  def fromJson(json: String): RedisSourceOffset = {
+    Serialization.read[RedisSourceOffset](json)
+  }
 }
 
 case class RedisConsumerOffset(groupName: String, offset: String)
