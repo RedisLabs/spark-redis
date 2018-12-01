45a1c84fc1a63c136b96df9d9e311ebd8cc40ffb
The Viet Nguyen <ntviet18@gmail.com>
2018-12-01 14:19:15 +0200

    Lift Redis source offset by stream

--- src/main/scala/org/apache/spark/sql/redis/stream/RedisSource.scala
+++ src/main/scala/org/apache/spark/sql/redis/stream/RedisSource.scala
@@ -6,6 +6,7 @@ import com.redislabs.provider.redis.util.{Logging, ParseUtils}
 import org.apache.spark.sql.catalyst.InternalRow
 import org.apache.spark.sql.execution.streaming.{Offset, Source}
 import org.apache.spark.sql.redis.StreamOptionStreamKey
+import org.apache.spark.sql.redis.stream.RedisSourceOffset.RedisConsumerOffset
 import org.apache.spark.sql.types.StructType
 import org.apache.spark.sql.{DataFrame, SQLContext}
 import org.apache.spark.unsafe.types.UTF8String
@@ -37,16 +38,24 @@ class RedisSource(sqlContext: SQLContext, metadataPath: String,
       val info = conn.xinfo(XINFO.StreamKey, streamKey)
       info.get(XINFO.LastGeneratedId)
         .map {
-          case index: String =>
-            RedisSourceOffset(index)
+          case offset: String =>
+            RedisSourceOffset(Map(streamKey -> RedisConsumerOffset("group55", offset)))
         }
     }
   }
 
   override def getBatch(start: Option[Offset], end: Offset): DataFrame = {
     val localSchema = currentSchema
-    val startJson = start.map(_.json())
-    val offsetRange = RedisSourceOffsetRange(streamKey, startJson, end.json())
+    val offsetStart = start.map(_.asInstanceOf[RedisSourceOffset])
+      .flatMap { offset =>
+        offset.offsets.get(streamKey)
+      }
+      .map { consumerOffset =>
+        consumerOffset.offset
+      }
+    val offsetEnd = end.asInstanceOf[RedisSourceOffset]
+      .offsets.get(streamKey).map(_.offset).get
+    val offsetRange = RedisSourceOffsetRange(streamKey, offsetStart, offsetEnd)
     val internalRdd = new RedisSourceRdd(sc, redisConfig, offsetRange)
       .map { case (id, fields) =>
         val fieldMap = fields.asScala.toMap + ("_id" -> id.toString)
--- src/main/scala/org/apache/spark/sql/redis/stream/RedisSourceOffset.scala
+++ src/main/scala/org/apache/spark/sql/redis/stream/RedisSourceOffset.scala
@@ -1,16 +1,20 @@
 package org.apache.spark.sql.redis.stream
 
+import com.redislabs.provider.redis.util.JsonUtils
 import org.apache.spark.sql.execution.streaming.Offset
+import org.apache.spark.sql.redis.stream.RedisSourceOffset.RedisConsumerOffset
 
 /**
   * @author The Viet Nguyen
   */
-case class RedisSourceOffset(offset: String) extends Offset {
+case class RedisSourceOffset(offsets: Map[String, RedisConsumerOffset]) extends Offset {
 
-  override def json(): String = offset
+  override def json(): String = JsonUtils.toJson(offsets)
 }
 
 case object RedisSourceOffset {
+
+  case class RedisConsumerOffset(groupName: String, offset: String)
 
   case object Smallest extends Offset {
 
@@ -21,4 +25,5 @@ case object RedisSourceOffset {
 
     override def json(): String = "+"
   }
+
 }
