585bd4bb5786748d4554cbc304b5fcedbb0142f9
The Viet Nguyen <ntviet18@gmail.com>
2018-12-09 15:27:10 +0200

    Reuse stream readers

--- src/main/scala/org/apache/spark/sql/redis/stream/RedisSource.scala
+++ src/main/scala/org/apache/spark/sql/redis/stream/RedisSource.scala
@@ -2,7 +2,6 @@ package org.apache.spark.sql.redis.stream
 
 import com.redislabs.provider.redis.RedisConfig
 import com.redislabs.provider.redis.util.ConnectionUtils.{JedisExt, XINFO, withConnection}
-import com.redislabs.provider.redis.util.StreamUtils.EntryIdEarliest
 import com.redislabs.provider.redis.util.{Logging, ParseUtils}
 import org.apache.spark.sql.catalyst.InternalRow
 import org.apache.spark.sql.execution.streaming.{Offset, SerializedOffset, Source}
@@ -10,7 +9,6 @@ import org.apache.spark.sql.redis.StreamOptionStreamKey
 import org.apache.spark.sql.types.StructType
 import org.apache.spark.sql.{DataFrame, SQLContext}
 import org.apache.spark.unsafe.types.UTF8String
-import redis.clients.jedis.{EntryID, PendingEntry}
 
 import scala.collection.JavaConverters._
 
@@ -82,34 +80,14 @@ class RedisSource(sqlContext: SQLContext, metadataPath: String,
         RedisSourceOffset.fromJson(json)
     }
     val offsetEnd = offsetEnds.offsets(streamKey)
+    val offsetRange = RedisSourceOffsetRange(streamKey, offsetEnd.groupName, None, offsetEnd.offset)
     withConnection(redisConfig.connectionForKey(streamKey)) { conn =>
-      val targetOffsetEndId = new EntryID(offsetEnd.offset)
-      val groupName = offsetEnd.groupName
-      val initialOffsetStartId = conn.xpending(streamKey, groupName, EntryIdEarliest,
-        targetOffsetEndId, 1, "consumer-123").asScala
-        .collectFirst { case pe: PendingEntry => pe.getID }
-        .getOrElse(EntryIdEarliest)
-      val initialStreamEntries = conn.xrange(streamKey, initialOffsetStartId, targetOffsetEndId,
-        1000).asScala
-      Stream.iterate(initialOffsetStartId -> initialStreamEntries) { case (prevId, _) =>
-        val offsetStartId = new EntryID(prevId.getTime, prevId.getSequence + 1)
-        val streamEntries = conn.xrange(streamKey, offsetStartId, targetOffsetEndId, 1000).asScala
-        val offsetEndId =
-          if (streamEntries.isEmpty) {
-            targetOffsetEndId
-          } else {
-            streamEntries.last.getID
-          }
-        offsetEndId -> streamEntries
-      }
-        .takeWhile { case (_, streamEntries) =>
-          streamEntries.nonEmpty
-        }
-        .map { case (_, streamEntries) =>
-          streamEntries.map(_.getID)
-        }
-        .foreach { entryIds =>
-          conn.xack(streamKey, groupName, entryIds: _*)
+      RedisStreamReader.pendingMessages(conn, offsetRange)
+        .map { entries => entries._1 }
+        .grouped(100)
+        .foreach { entries =>
+          conn.xack(streamKey, offsetEnd.groupName, entries: _*)
+          logDebug(s"Committed entries: $entries")
         }
     }
   }
