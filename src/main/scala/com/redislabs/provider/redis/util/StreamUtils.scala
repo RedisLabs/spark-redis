9d6607ceb38ab558cc549e41d3bca6f3f387b00e
The Viet Nguyen <ntviet18@gmail.com>
2018-12-15 09:26:46 +0200

    Simplify reset existing consumer group

--- src/main/scala/com/redislabs/provider/redis/util/StreamUtils.scala
+++ src/main/scala/com/redislabs/provider/redis/util/StreamUtils.scala
@@ -11,27 +11,16 @@ object StreamUtils extends Logging {
   val EntryIdEarliest = new EntryID(0, 0)
 
   def createConsumerGroupIfNotExist(conn: Jedis, streamKey: String, groupName: String,
-                                    offset: EntryID): Unit = {
-    createConsumerGroupIfNotExistOrElse(conn, streamKey, groupName, offset) {
-      // ignore existing group
-    }
-  }
-
-  def createConsumerGroupIfNotExistOrElseReset(conn: Jedis, streamKey: String, groupName: String,
-                                               offset: EntryID): Unit = {
-    createConsumerGroupIfNotExistOrElse(conn, streamKey, groupName, offset) {
-      conn.xgroupSetID(streamKey, groupName, offset)
-    }
-  }
-
-  private def createConsumerGroupIfNotExistOrElse(conn: Jedis, streamKey: String, groupName: String,
-                                                  offset: EntryID)(orElse: => Unit): Unit = {
+                                    offset: EntryID, reset: Boolean = false): Unit = {
     try {
       conn.xgroupCreate(streamKey, groupName, offset, true)
     } catch {
       case e: Exception if StringUtils.contains(e.getMessage, "already exists") =>
         logInfo(s"Consumer group exists: $groupName")
-        orElse()
+        if (reset) {
+          logInfo(s"Reset consumer group: $groupName")
+          conn.xgroupSetID(streamKey, groupName, offset)
+        }
     }
   }
 }
