88f5bd29aedf8b397d2831ece6e94e1ad5dba3ec
The Viet Nguyen <ntviet18@gmail.com>
2018-12-26 19:20:38 +0200

    Reference stream keys parameter

--- src/main/scala/org/apache/spark/sql/redis/stream/RedisSourceConfig.scala
+++ src/main/scala/org/apache/spark/sql/redis/stream/RedisSourceConfig.scala
@@ -11,7 +11,7 @@ object RedisSourceConfig {
 
   def fromMap(config: Map[String, String]): RedisSourceConfig = {
     val streamKey = config.getOrElse(StreamOptionStreamKeys,
-      throw new IllegalArgumentException("Please specify 'stream.key'"))
+      throw new IllegalArgumentException(s"Please specify '$StreamOptionStreamKeys'"))
     val consumerConfigs = Seq(RedisConsumerConfig(streamKey, "group55", "consumer-123", 100, 500))
     RedisSourceConfig(consumerConfigs)
   }
