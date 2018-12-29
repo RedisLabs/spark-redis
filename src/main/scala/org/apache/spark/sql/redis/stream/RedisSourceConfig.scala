33f8172588f950cfaf3683c4922b4ba23bb67c7d
The Viet Nguyen <ntviet18@gmail.com>
2018-12-29 16:55:37 +0200

    Reorder parameter parsing

--- src/main/scala/org/apache/spark/sql/redis/stream/RedisSourceConfig.scala
+++ src/main/scala/org/apache/spark/sql/redis/stream/RedisSourceConfig.scala
@@ -11,11 +11,11 @@ case class RedisSourceConfig(consumerConfigs: Seq[RedisConsumerConfig])
 object RedisSourceConfig {
 
   def fromMap(config: Map[String, String]): RedisSourceConfig = {
-    val groupName = config.getOrElse(StreamOptionGroupName, "group55")
-    val consumerPrefix = config.getOrElse(StreamOptionConsumerPrefix, "consumer")
-    val parallelism = config.get(sql.redis.StreamOptionParallelism).map(_.toInt).getOrElse(1)
     val streamKeys = config.getOrElse(StreamOptionStreamKeys,
       throw new IllegalArgumentException(s"Please specify '$StreamOptionStreamKeys'"))
+    val parallelism = config.get(sql.redis.StreamOptionParallelism).map(_.toInt).getOrElse(1)
+    val groupName = config.getOrElse(StreamOptionGroupName, "group55")
+    val consumerPrefix = config.getOrElse(StreamOptionConsumerPrefix, "consumer")
     val consumerConfigs = streamKeys.split(",").flatMap { streamKey =>
       (1 to parallelism).map { consumerIndex =>
         RedisConsumerConfig(streamKey, s"$groupName", s"$consumerPrefix-$consumerIndex", 100, 500)
