fa8288c9230951828b177223bcf59eac09e1309b
alanroch <alan_roche@mcafee.com>
2019-03-14 16:08:09 +0000

    Issue#140 Add support for SSL
    https://github.com/RedisLabs/spark-redis/issues/140

--- src/main/scala/com/redislabs/provider/redis/package.scala
+++ src/main/scala/com/redislabs/provider/redis/package.scala
@@ -1,7 +1,7 @@
 package com.redislabs.provider
 
 package object redis extends RedisFunctions {
-
+  val RedisSslScheme: String = "rediss"
   val RedisDataTypeHash: String = "hash"
   val RedisDataTypeString: String = "string"
 }
