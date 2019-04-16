efa50f5659cc408e0bd4458760f48f224f40bb90
Oleksiy Dyagilev <aka.fe2s@gmail.com>
2019-04-16 11:47:53 +0200

    Fix scalastyle warnings #150 (#152)

--- src/test/scala/com/redislabs/provider/redis/SparkStreamingRedisSuite.scala
+++ src/test/scala/com/redislabs/provider/redis/SparkStreamingRedisSuite.scala
@@ -1,6 +1,7 @@
 package com.redislabs.provider.redis
 
 import com.redislabs.provider.redis.env.Env
+import com.redislabs.provider.redis.util.Logging
 import org.apache.spark.sql.SparkSession
 import org.apache.spark.streaming.{Seconds, StreamingContext}
 import org.scalatest.{BeforeAndAfterEach, FunSuite}
@@ -8,7 +9,7 @@ import org.scalatest.{BeforeAndAfterEach, FunSuite}
 /**
   * For spark streaming test we have to create spark and streaming context for each test
   */
-trait SparkStreamingRedisSuite extends FunSuite with Env with BeforeAndAfterEach {
+trait SparkStreamingRedisSuite extends FunSuite with Env with BeforeAndAfterEach with Logging {
 
   override protected def beforeEach(): Unit = {
     super.beforeEach()
--- src/test/scala/org/apache/spark/sql/redis/stream/RedisSourceConfigSuite.scala
+++ src/test/scala/org/apache/spark/sql/redis/stream/RedisSourceConfigSuite.scala
@@ -8,40 +8,42 @@ import org.scalatest.{FunSuite, Matchers}
   */
 class RedisSourceConfigSuite extends FunSuite with Matchers {
 
+  val group: String = "group55"
+
   test("testFromMap") {
     val config = RedisSourceConfig.fromMap(Map(
       StreamOptionStreamKeys -> "mystream1,mystream2,mystream3",
       StreamOptionStreamOffsets ->
-        """
+        s"""
           |{
           |  "offsets":{
           |    "mystream1": {
-          |      "groupName": "group55",
+          |      "groupName": "$group",
           |      "offset": "0-10"
           |    },
           |    "mystream2": {
-          |       "groupName": "group55",
+          |       "groupName": "$group",
           |       "offset": "0-7"
           |    }
           |  }
           |}
         """.stripMargin,
       StreamOptionParallelism -> "2",
-      StreamOptionGroupName -> "group55",
+      StreamOptionGroupName -> group,
       StreamOptionConsumerPrefix -> "consumer"
     ))
     config shouldBe RedisSourceConfig(
       Seq(
-        RedisConsumerConfig("mystream1", "group55", "consumer-1", 100, 500),
-        RedisConsumerConfig("mystream1", "group55", "consumer-2", 100, 500),
-        RedisConsumerConfig("mystream2", "group55", "consumer-1", 100, 500),
-        RedisConsumerConfig("mystream2", "group55", "consumer-2", 100, 500),
-        RedisConsumerConfig("mystream3", "group55", "consumer-1", 100, 500),
-        RedisConsumerConfig("mystream3", "group55", "consumer-2", 100, 500)
+        RedisConsumerConfig("mystream1", group, "consumer-1", 100, 500),
+        RedisConsumerConfig("mystream1", group, "consumer-2", 100, 500),
+        RedisConsumerConfig("mystream2", group, "consumer-1", 100, 500),
+        RedisConsumerConfig("mystream2", group, "consumer-2", 100, 500),
+        RedisConsumerConfig("mystream3", group, "consumer-1", 100, 500),
+        RedisConsumerConfig("mystream3", group, "consumer-2", 100, 500)
       ),
       Some(RedisSourceOffset(Map(
-        "mystream1" -> RedisConsumerOffset("group55", "0-10"),
-        "mystream2" -> RedisConsumerOffset("group55", "0-7")
+        "mystream1" -> RedisConsumerOffset(group, "0-10"),
+        "mystream2" -> RedisConsumerOffset(group, "0-7")
       )))
     )
   }
