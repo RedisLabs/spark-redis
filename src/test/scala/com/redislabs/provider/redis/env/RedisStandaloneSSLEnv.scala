1f44e2a5cd95724a30d2d5ed3ff4eb6f3ea0b04b
Guy Korland <gkorland@gmail.com>
2020-05-19 22:59:19 +0300

    add ssl connection test (#244)
    * add ssl connection test
    * add keystore

---
+++ src/test/scala/com/redislabs/provider/redis/env/RedisStandaloneSSLEnv.scala
@@ -0,0 +1,21 @@
+package com.redislabs.provider.redis.env
+
+import com.redislabs.provider.redis.{RedisConfig, RedisEndpoint}
+import org.apache.spark.SparkConf
+
+trait RedisStandaloneSSLEnv extends Env {
+
+  override val redisPort = 6380
+  
+  override val conf: SparkConf = new SparkConf()
+    .setMaster("local[*]").setAppName(getClass.getName)
+    .set("spark.redis.host", redisHost)
+    .set("spark.redis.port", s"$redisPort")
+    .set("spark.redis.auth", redisAuth)
+    .set("spark.redis.ssl", "true")
+    .set("spark.streaming.stopGracefullyOnShutdown", "true")
+    .set("spark.driver.bindAddress", "127.0.0.1")
+
+  override val redisConfig: RedisConfig =
+    new RedisConfig(RedisEndpoint(redisHost, redisPort, redisAuth, ssl=true))
+}
